f81cbfb0e4e00eb58b9e4c8c8b95971e72953a37
Kaelin Ellis <ellis.kaelin@gmail.com>
2025-05-19 16:13:15 -0800

    started modeling cfp equipment

---
+++ equip_cfp/v5412b.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+from pysep.separator import SepThreePhase
+
+oil_props = {
+    "mass_flow": 1.404e5 / 3,  # lbm/hr
+    "density": 58.74,  # lbm/ft3, 100 PSIG and 120 deg F
+    "viscosity": 52,  # centipoise
+    "drop_io": np.nan,
+    "drop_iw": 200,  # micron, smallest oil droplet to be removed from water
+    "drop_ig": 100,  # micron, oil in gas, 140 and below can be caught by demister pad
+}
+
+wat_props = {
+    "mass_flow": 1.482e6 / 3,  # lbm/hr
+    "density": 62.46,  # lbm/ft3, 350 PSIG and 150 deg F, is this accurate?
+    "viscosity": 0.75,  # centipoise
+    "drop_io": 500,  # micron, smallest droplet to be removed in the process
+    "drop_iw": np.nan,
+    "drop_ig": 100,  # micron, water in gas, 140 and below can be caught by demister pad
+}
+
+gas_props = {
+    "mass_flow": 4 * 982,  # lbm/hr
+    "density": 0.9444,  # lbm/ft3
+    "viscosity": 1.327e-2,  # centipoise
+    "drop_io": 140,  # micron, smallest droplet to be removed in the process, 140 can be caught by demister
+    "drop_iw": 140,  # micron, smallest droplet to be removed
+    "drop_ig": np.nan,
+}
+
+# primary dimensions
+vid = 8  # feet, this the ID, 9.5
+lss = 30  # feet, actually 45 (for MEG, for similiar rates, we were looking at 45 feet...)
+leff = 0.8 * lss
+# liq_frac = 0.8
+hoil = 6.5  # hhll is at seven feet
+hwat = 5  # right at the "weir" height
+hgas = vid - hoil
+
+primary = SepThreePhase(vid, lss, leff, hoil, hwat, oil_props, wat_props, gas_props)
+print(primary)
+primary.results()
+
+mawp = 100  # psig, not updated
+wall_thk = primary.shell_thick(mawp)
+bare_wgt = primary.weight_bare(wall_thk)
+xtra_wgt = 25000  # lbm, weight of vessel internals and nozzles
+vssl_wgt = bare_wgt + xtra_wgt
+
+print(f"Primary MAWP: {mawp} psig, Wall Thick: {round(wall_thk, 2)} inches, Weight: {round(vssl_wgt/2000, 2)} tons\n")
+
+coal_len = primary.coal_plate_length(150, pf=0.0)
+print(f"Length of the Coalescing Plate is {round(coal_len, 2)} ft")
+
+
+# note, the V-5411 is around 10' in ID and 22' in length
+# how does this compare to the various POP vessels?
+# POPS Vessel is 10' ID and 40' long...so twice as big...
---
+++ pysep/units.py
@@ -0,0 +1,41 @@
+"""Code for Unit Conversions"""
+
+
+def volm_mass_conversion(volm_flow: float, units: str, rho_fld: float) -> float:
+    """Volumetric to Mass Flow Conversion
+
+    Convert from a volumetric flow in units of bpd, gpm, mscfd or mmscfd
+    to a mass flow, in lbm/hour. If using mscfd or mmscfd, need to use the
+    density that is at standard conditions.
+
+    Args:
+        volm_flow (float): Volumetric Flow
+        units (str): bpd, gpm, mscfd or mmscfd
+        rho_fld (float): Density of Fluid, lbm/ft3
+
+    Returns:
+        mass_flow (float): Mass Flow, lbm/hr
+    """
+    conv_dict = {"bpd": 42 / (24 * 7.48), "gpm": 60 / 7.48, "mscfd": 1e3 / 24, "mmscfd": 1e6 / 24}
+    mass_flow = volm_flow * conv_dict[units] * rho_fld
+    return mass_flow
+
+
+if __name__ == "__main__":
+    oil_volm = 10000  # bpd
+    oil_unit = "bpd"
+    oil_rho = 58.74  # lbm/ft3
+    oil_mass = volm_mass_conversion(oil_volm, oil_unit, oil_rho)
+    print(f"\nOil Mass Flow is {oil_mass:.3e} lbm/ft3")
+
+    water_volm = 11000  # bpd
+    water_unit = "bpd"
+    water_rho = 62.4  # lbm/ft3
+    water_mass = volm_mass_conversion(water_volm, water_unit, water_rho)
+    print(f"Water Mass Flow is {water_mass:.3e} lbm/ft3")
+
+    gas_volm = 40  # mmscfd
+    gas_unit = "mmscfd"
+    gas_rho = 0.0415  # standard conditions, lbm/ft3
+    gas_mass = volm_mass_conversion(gas_volm, "mmscfd", gas_rho)
+    print(f"Gas Mass Flow is {gas_mass:.3e} lbm/ft3\n")
